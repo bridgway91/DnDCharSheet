374633c3d9e09af20f0584e5bd5936c2d4526a9d
Brandon Ridgway <bridgway91@gmail.com>
2025-02-12 23:20:19 -0500

    decent chunk of JS done today, got most info saves to character object, just spells, backstory, and campaign notes left... then have to add in all the button functionality, from adding options to import/export

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 class Character {
     constructor() {
         this.name = ''
-        this.characteristics = [] // length=9 : race, background, alignment, age, height, weight, eyes, skin, hair
         this.class = [[]] // length=X -- array of arrays, each one separate [class,level]
+        this.characteristics = [] // length=9 : race, background, alignment, age, height, weight, eyes, skin, hair
         this.stats = [] // length=6 : str, dex, con, int, wis, cha
         this.inspiration = false
         this.proficiency = 2
-        this.saves = [] // length=6 -- just booleans of if prof or not
-        this.skills = [] // array of skills, NOT BOOLEAN, instead 0/1/2 for * prof bonus (to account for expertise)
+        this.saves = [] // length=6 -- [[boolean if prof, # bonus],...]
+        this.skills = [] // array of skills, NOT BOOLEAN, instead 0/1/2 for * prof bonus (to account for expertise) -- [[0/1/2, # bonus],...]
         this.weapons = '' // strings of weapon names, tho mainly simple / martial
         this.armor = ''
         this.tools = ''
         this.languages = ''
-        this.armor = 10
+        this.armorClass = 10
         this.initiative = 0
         this.speed = 30
-        this.maxHealth = 10
-        this.currentHealth = 10
-        this.tempHealth = 0
-        this.hit_die = [[1,'1d10']] // [# dice, die size], double array to account for if have multiple hit die sizes
-        this.death_saves = [[0,0,0],[0,0,0]]
+        this.healthMax = 10
+        this.healthCurrent = 10
+        this.healthTemp = 0
+        this.hitDie = [[1,'1d10']] // [# dice, die size], double array to account for if have multiple hit die sizes
+        this.deathSaves = [[0,0,0],[0,0,0]]
         this.attacks = [['Unarmed Strike',2,'1d4+2 B']] // name,bonus,dmg
         this.money = [0,0,0,0,0] // platinum, gold, etherium, silver, copper
-        this.equipment = [[]] // each item is an array [amount, name/description]
+        this.items = [[]] // each item is an array [amount, name/description]
         this.trackers = [[],[]] // each [] is ['name',#-current,#-max]
         this.features = [[]] // each feature is array [title, source, description]
         this.spellcasting = [] // class, ability, save DC, attack bonus
@@ -42,7 +42,7 @@ const imChar = document.querySelector('#importChar')
 const exChar = document.querySelector('#exportChar')
 
 const charName = document.querySelector('#characterName')
-const charClasses = document.querySelector('#characterClasses')
+const charClasses = document.querySelector('#classes')
 const charRace = document.querySelector('#race')
 const charBackground = document.querySelector('#background')
 const charAlignment = document.querySelector('#alignment')
@@ -52,6 +52,27 @@ const charWeight = document.querySelector('#weight')
 const charEyes = document.querySelector('#eyes')
 const charSkin = document.querySelector('#skin')
 const charHair = document.querySelector('#hair')
+
+const charStats = document.querySelector('#stats')
+
+const charInspiration = document.querySelector('#inspiration')
+const charProficiency = document.querySelector('#proficiencyBonus')
+const charSaves = document.querySelector('#saves')
+const charSkills = document.querySelector('#skills')
+const charOther = document.querySelector('#other')
+
+const charArmorClass = document.querySelector('#armorClass')
+const charInitiative = document.querySelector('#initiative')
+const charSpeed = document.querySelector('#speed')
+const charHealth = document.querySelector('#health')
+const charHitDice = document.querySelector('#hitDiceWrapper')
+const charDeathSaves = document.querySelector('#deathSaves')
+const charAttacks = document.querySelector('#attacks')
+const charMoney = document.querySelector('#money')
+const charItems = document.querySelector('#itemsWrapper')
+
+const charTrackers = document.querySelector('#trackers')
+const charFeatures = document.querySelector('#features')
 
 /////////////////////////////////////////////////
 
@@ -82,14 +103,6 @@ function editCharInfo() { // enables editing all inputs
 }
 
 function saveCharInfo() { // 2 parts : closes off all info that doesn't change often from being edited, and saves all input info to myChar object (+ sets to localStorage)
-    // myChar.name = charName.value
-    // //myChar.class...
-    // myChar.characteristics = [charAge.value,charHeight.value,charWeight.value,charEyes.value,charSkin.value,charHair.value]
-    // myChar.race = charRace.value
-    // myChar.background = charBackground.value
-    // myChar.alignment = charAlignment.value
-    // console.log(myChar)
-    // localStorage.setItem('character',JSON.stringify(myChar))
     const allInput = document.querySelectorAll('input')
     const allButton = document.querySelectorAll('button')
     for (let i of allInput) {
@@ -98,10 +111,9 @@ function saveCharInfo() { // 2 parts : closes off all info that doesn't change o
     for (let i of allButton) {
         if(!i.classList.contains('changing')) {i.disabled = true}
     }
-}
 
-function stopEdit() {
-
+    updateCharacter()
+    updateDerivedValues()
 }
 
 function grabLocal() { // if a character exists in localstorage, grabs it and assigns all relevant values to sheet
@@ -110,4 +122,71 @@ function grabLocal() { // if a character exists in localstorage, grabs it and as
 
 function updateDerivedValues() { // default just the general stat modifiers (str, con, etc) to avoid interfering with temp / specific bonuses to saves or skills or w/e else
 
+}
+
+function updateCharacter() { // updates myChar with entered info
+    // name
+    myChar.name = charName.value
+    // classes
+    let classDivsArray = [...charClasses.querySelectorAll('div')]
+    myChar.class = classDivsArray.map(e=>[...e.querySelectorAll('input')].map(f=>f.value))
+    // characteristics
+    myChar.characteristics = [charRace.value, charBackground.value,charAlignment.value,charAge.value,charHeight.value,charWeight.value,charEyes.value,charSkin.value,charHair.value]
+    // stats
+    let statInputArray = [...charStats.querySelectorAll('input')]
+    myChar.stats = statInputArray.map(e=>e.value)
+    // inspiration
+    myChar.inspiration = charInspiration.checked
+    // proficiency
+    myChar.proficiency = charProficiency.value
+    // saves
+    let savesCheckboxArray = [...charSaves.querySelectorAll('input[type=checkbox]')]
+    let savesNumberArray = [...charSaves.querySelectorAll('input[type=number]')]
+    myChar.saves = savesCheckboxArray.map((e,i)=>[e.checked,savesNumberArray[i].value])
+    // skills
+    let skillsDivArray = [...charSkills.querySelectorAll('div')]
+    let skillsNumberArray = [...charSkills.querySelectorAll('input[type=number')]
+    skillsDivArray = skillsDivArray.map(e=>[...e.querySelectorAll('input')].map(f=>f.checked).indexOf(true))
+    myChar.skills = skillsDivArray.map((e,i)=>[e,skillsNumberArray[i].value])
+    // weapons
+    myChar.weapons = charOther.querySelector('#weaponProf').value
+    // armor
+    myChar.armor = charOther.querySelector('#armorProf').value
+    // tools
+    myChar.tools = charOther.querySelector('#toolProf').value
+    // languages
+    myChar.languages = charOther.querySelector('#languageProf').value
+    // armor class
+    myChar.armorClass = charArmorClass.value
+    // initiative
+    myChar.initiative = charInitiative.value
+    // speed
+    myChar.speed = charSpeed.value
+    // health
+    myChar.healthMax = charHealth.querySelector('#hpMax').value
+    myChar.healthCurrent = charHealth.querySelector('#hpCurrent').value
+    myChar.healthTemp = charHealth.querySelector('#hpTemp').value
+    // hit dice
+    let hitDiceDivArray = [...charHitDice.querySelectorAll('div')]
+    myChar.hitDie = hitDiceDivArray.map(e=>[...e.querySelectorAll('input')].map(f=>f.value))
+    // death saves
+    myChar.deathSaves[0] = [...charDeathSaves.querySelector('#dsPass').querySelectorAll('input')].map(e=>e.checked)
+    myChar.deathSaves[1] = [...charDeathSaves.querySelector('#dsFail').querySelectorAll('input')].map(e=>e.checked)
+    // attacks
+    let attacksArray = [...charAttacks.querySelector('tbody').querySelectorAll('tr')]
+    myChar.attacks = attacksArray.map(e=>[...e.querySelectorAll('input')].map(f=>f.value))
+    // money
+    myChar.money = [...charMoney.querySelector('tbody').querySelectorAll('input')].map(e=>e.value)
+    // items
+    let itemsArray = [...charItems.querySelectorAll('div')]
+    myChar.items = itemsArray.map(e=>[...e.querySelectorAll('input')].map(f=>f.value))
+    // trackers
+    let trackerArray = [...charTrackers.querySelectorAll('div')]
+    myChar.trackers = trackerArray.map(e=>[...e.querySelectorAll('input')].map(f=>f.value))
+    // features
+    let featureArray = [...charFeatures.querySelectorAll('div')]
+    myChar.features = featureArray.map(e=>[...e.querySelectorAll('*')].map(f=>f.value))
+
+    console.log(myChar)
+    localStorage.setItem('character',JSON.stringify(myChar))
 }
